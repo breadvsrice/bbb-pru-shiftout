a9a6046fe9cb5b9ef120e884902962eaba1435e6
breadvsrice <darrencoste@gmail.com>
2023-05-20 01:02:30 +0900

    Create regedit.c

---
+++ regedit.c
@@ -0,0 +1,23 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+
+        printf("Preparing to open /dev/mem with R/W permissions...\n");
+
+        int fd = open("/dev/mem", O_RDWR);
+    if (fd == -1) {
+        // Handle error opening /dev/mem
+                printf("Handle Error...\n");
+        return -1;
+    }
+
+    // Perform operations on /dev/mem using the file descriptor
+        printf("Closing /dev/mem instance...\n");
+    close(fd);
+
+        printf("Program Complete!\n");
+
+    return 0;
+}
